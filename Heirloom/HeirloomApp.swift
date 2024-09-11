04a1409013999af7e9d94adfad73391910f83fc2
TylerBrodsky <tyler.c.brodsky@gmail.com>
2024-09-10 22:37:11 -0700

    Initial Commit

---
+++ Heirloom/HeirloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeirloomApp.swift
+//  Heirloom
+//
+//  Created by Tyler Brodsky on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HeirloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
